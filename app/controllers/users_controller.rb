1b7f69e2806b1958da8cbf9566c3d85462c63c57
Amal T S <amal.ts@gemessence.com>
2024-07-08 17:15:37 +0530

    Initial Commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,68 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :show]
+
+  def index
+    @users = User.all
+    respond_to do |format|
+      format.json do
+        render json: @users
+      end
+
+      format.html
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def show
+    respond_to do |format|
+      format.json do
+        render json: @user
+      end
+      format.html
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      format.json do
+        if @user.save
+          render json: @user, status: :created
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      format.html do
+        if @user.save
+          redirect_to user_path(@user), notice: 'User was successfully created.'
+        else
+          render :new
+        end
+      end
+    end
+  end
+
+  def filter
+    campaign_names = params[:campaign_names].gsub(/\s+/, "").split(',')
+    @users = User.where("JSON_CONTAINS(campaigns_list->'$[*].campaign_name', ?)", campaign_names.to_json)
+    render json: @users
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, campaigns_list: [:campaign_name, :campaign_id])
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
